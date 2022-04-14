711eeab92ca62cbd881e160124f401452b362d9c
luong <luongvdhe150026@fpt.edu.vn>
2022-04-14 23:01:54 +0700

    add project

---
+++ Project_prj_bl5/src/java/model/Absent.java
@@ -0,0 +1,95 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author Vu Dai Luong
+ */
+public class Absent {
+       private int stt;
+    private String id;
+    private int dayOff;
+    private Date start;
+    private Date end;
+    private String fullName;
+    private String imgUrl;
+
+    public Absent() {
+    }
+
+    public Absent(int stt, String id, int dayOff, Date start, Date end, String fullName, String imgUrl) {
+        this.stt = stt;
+        this.id = id;
+        this.dayOff = dayOff;
+        this.start = start;
+        this.end = end;
+        this.fullName = fullName;
+        this.imgUrl = imgUrl;
+    }
+
+    public int getStt() {
+        return stt;
+    }
+
+    public void setStt(int stt) {
+        this.stt = stt;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getDayOff() {
+        return dayOff;
+    }
+
+    public void setDayOff(int dayOff) {
+        this.dayOff = dayOff;
+    }
+
+    public Date getStart() {
+        return start;
+    }
+
+    public void setStart(Date start) {
+        this.start = start;
+    }
+
+    public Date getEnd() {
+        return end;
+    }
+
+    public void setEnd(Date end) {
+        this.end = end;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    @Override
+    public String toString() {
+        return "Absent{" + "stt=" + stt + ", id=" + id + ", dayOff=" + dayOff + ", start=" + start + ", end=" + end + ", fullName=" + fullName + ", imgUrl=" + imgUrl + '}';
+    }
+}
