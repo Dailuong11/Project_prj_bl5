d315f647f6eae787c5b1075bf981c587bfa383ba
luong <luongvdhe150026@fpt.edu.vn>
2022-04-21 22:55:02 +0700

    add project

---
+++ Project_prj_bl5/src/java/Filter/AuthorationFilter.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Filter;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.io.IOException;
+
+import model.Account;
+
+/**
+ *
+ * @author Apple
+ */
+@WebFilter(filterName = "AuthorationFilter", urlPatterns = {"/admin/*"})
+public class AuthorationFilter implements Filter {
+    /**
+     *
+     * @param request The servlet request we are processing
+     * @param response The servlet response we are creating
+     * @param chain The filter chain we are processing
+     *
+     * @exception IOException if an input/output error occurs
+     * @exception ServletException if a servlet error occurs
+     */
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response,
+            FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+        HttpSession session = req.getSession();
+        //Kiểm tra đăng nhập
+        Account account = (Account) session.getAttribute("acc");
+        if (account != null && account.getRole().equals(Account.ADMIN)) {
+            //cho qua
+            chain.doFilter(request, response);
+            return;
+        }
+         res.sendRedirect("http://localhost:8080/Assignment/login");
+    }
+    
+    /**
+     * Destroy method for this filter
+     */
+    @Override
+    public void destroy() {        
+    }
+
+    /**
+     * Init method for this filter
+     * @param filterConfig
+     */
+    @Override
+    public void init(FilterConfig filterConfig) {        
+      
+    }
+
+    
+}
